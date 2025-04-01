5f7ebaa90535caafa8ac6f6e4c5c85bb3bc544cc
Steven Halstead <steven.g.halstead@gmail.com>
2025-03-31 21:34:37 -0400

    evaluate win/loss of round

--- app/main.py
+++ app/main.py
@@ -28,6 +28,7 @@ def print_instructions():
 
     print("Welcome to RPSLS!")
     print(INSTRUCTIONS)
+    print(f"Valid game commands are: {COMMANDS}\n")
     print(f"Your possible actions are: {ACTIONS}\n")
     print("Each action wins against two other actions as follows:")
     pprint.pprint(RULES)
@@ -46,21 +47,36 @@ def computer_action():
     return ACTIONS[random.randint(0, len(ACTIONS) - 1)]
 
 
+def evaluate_round(user_action, comp_action):
+    """Compare user and computer actions, determine who won the round"""
+
+    if user_action == comp_action:
+        print("Tie!")
+    elif comp_action in RULES[user_action]:
+        print("You won the round!")
+    elif user_action in RULES[comp_action]:
+        print("The computer won the round!")
+    else:
+        raise Exception("INVALID GAME STATE")
+
+
 def play_round():
     """Execute a single round of the game"""
 
-    user_input = input("\nChoose your action: ").lower()
+    user_action = input("\nChoose your action: ").lower()
 
-    while user_input not in ACTIONS + COMMANDS:
-        user_input = input("\nPlease choose a valid action or 'exit': ").lower()
+    while user_action not in ACTIONS + COMMANDS:
+        user_action = input("\nPlease choose a valid action or command: ").lower()
 
-    print(f"You chose: {user_input}")
+    print(f"You chose: {user_action}")
     
-    if user_input == "exit":
+    if user_action == "exit":
         return 0
     
     comp_action = computer_action()
     print(f"Computer chose: {comp_action}")
+
+    evaluate_round(user_action, comp_action)
     
     print()
     return 1
