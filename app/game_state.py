8b4549bcbdb0c7db092fb6bc8c7bd6da3a1ec18f
Steven Halstead <steven.g.halstead@gmail.com>
2025-04-01 09:16:46 -0400

    implement game state persistence

--- app/game_state.py
+++ app/game_state.py
@@ -1,4 +1,7 @@
 from pprint import pprint
+import json
+from pathlib import Path
+
 
 class dotdict(dict):
     """dot.notation access to dictionary attributes"""
@@ -14,13 +17,32 @@ class GameState:
     COMP_SCORE = "COMP_SCORE"
     TIES = "TIES"
 
+    DATA_STORE_FILE = 'game_state.json'
 
     def __init__(self):
-        self.state = dotdict({
-            self.PLAYER_SCORE: 0,
-            self.COMP_SCORE: 0,
-            self.TIES: 0
-        })
+        data_store = Path(self.DATA_STORE_FILE)
+        if data_store.is_file():
+            self.__read_state()
+        else:
+            self.state = dotdict({
+                self.PLAYER_SCORE: 0,
+                self.COMP_SCORE: 0,
+                self.TIES: 0
+            })
+
+
+    def __del__(self):
+        self.__write_state()
+
+
+    def __write_state(self):
+        with open(self.DATA_STORE_FILE, 'w') as file:
+            file.write(json.dumps(self.state))
+
+
+    def __read_state(self):
+        with open(self.DATA_STORE_FILE) as file:
+            self.state = dotdict(json.load(file))
 
 
     def print_scoreboard(self):
@@ -45,4 +67,5 @@ class GameState:
         self.state.PLAYER_SCORE = 0
         self.state.COMP_SCORE = 0
         self.state.TIES = 0
+        self.__write_state()
 
