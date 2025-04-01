9ab31ea750c7690e98c6ae98f4a5e220155aa1bb
Steven Halstead <steven.g.halstead@gmail.com>
2025-03-31 20:42:50 -0400

    basic user interaction loop

--- app/main.py
+++ app/main.py
@@ -1,7 +1,21 @@
 # Application execution starts from this file
 
+INSTRUCTIONS = """
+>> USER INSTRUCTIONS <<
+At any time type "exit" to exit the game.
+"""
+
 def main():
-    print("Hello World!")
+    print("Welcome to RPSLS!")
+    print(INSTRUCTIONS)
+
+    while True:
+        user_input = input("Choose your action: ").lower()
+        print(f"You chose: {user_input}\n")
+
+        if user_input == "exit":
+            print("Thanks for playing, bye!")
+            break
 
 if __name__ == "__main__":
     main()
